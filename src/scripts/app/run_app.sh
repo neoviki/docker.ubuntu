c711d08c38a86723598d29bcc78874394e8b371e
Vignesh Natarajan <vikiworks.io@gmail.com>
2021-07-05 11:21:57 +0200

    interactive and non interactive modes

--- src/scripts/app/run_app.sh
+++ src/scripts/app/run_app.sh
@@ -2,6 +2,11 @@ source ./app_config.sh
 CONTAINER_NAME="${DOCKER_IMAGE_NAME}.dapp"
 
 docker container stop $CONTAINER_NAME 1> /dev/null 2> /dev/null
+
+#Interactive Mode
 docker start -ai $CONTAINER_NAME
+
+#Non interactive Mode
+#docker start $CONTAINER_NAME
 
 
